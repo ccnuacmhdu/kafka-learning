da8d60cbd45bb21c1f9cad5fef2239e1f27942a9
study <study@example.com>
2022-06-23 01:11:42 +0800

    CustomProducerSync

---
+++ src/main/java/org/example/kafka/producer/CustomProducerSync.java
@@ -0,0 +1,27 @@
+package org.example.kafka.producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class CustomProducerSync {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+
+        for (int i = 0; i < 5; i++) {
+            // 同步发送
+            kafkaProducer.send(new ProducerRecord<>("first","kafkaProducer " + i)).get();
+        }
+
+        kafkaProducer.close();
+    }
+}
